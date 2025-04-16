a013b6619ab06b9e7133616d06f3a191d9efeb3a
phmuniz <phmcarvalho@hotmail.com.br>
2025-04-16 14:05:32 -0300

    fazendo rota para alterar senha

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -6,6 +6,7 @@ import { DetalhesUserController } from "./controllers/user/DetalhesUserControlle
 import { ObtemUsersController } from "./controllers/user/ObtemUsersController";
 import { estaAutenticado } from "./middlewares/estaAutenticado";
 import { DeleteUserController } from "./controllers/user/DeleteUserController";
+import { AlteraSenhaUserController } from "./controllers/user/AlteraSenhaUserController";
 
 const router = Router()
 
@@ -15,5 +16,6 @@ router.post('/sessao', new LogaUserController().handle)
 router.get('/eu', estaAutenticado,  new DetalhesUserController().handle)
 router.get('/users', estaAutenticado,  new ObtemUsersController().handle)
 router.delete('/users', estaAutenticado, new DeleteUserController().handle)
+router.put('/users', estaAutenticado, new AlteraSenhaUserController().handle)
 
 export { router }
