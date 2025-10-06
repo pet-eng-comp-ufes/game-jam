27cd616320e6a7a01dd58878873ee95b7b28df63
phmuniz <phmcarvalho@hotmail.com.br>
2025-10-06 12:00:32 -0300

    dockerizando aplicacao

--- backend/prisma/migrations/20250427191010_models/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "seasons" ALTER COLUMN "numero" SET DATA TYPE TEXT,
-ALTER COLUMN "numParticipantesPorEquipe" SET DATA TYPE TEXT;
--- backend/prisma/migrations/20251005205058_add_participante_fields/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "participantes" ADD COLUMN     "cpf" TEXT,
-ADD COLUMN     "curso" TEXT,
-ADD COLUMN     "instituicao" TEXT;
